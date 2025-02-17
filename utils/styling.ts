4046895a1f84b12b418296e7410e412d7346b5b4
remus-wolico <135813551+remus00@users.noreply.github.com>
2025-02-17 15:58:58 +0100

    initialize react native project

---
+++ utils/styling.ts
@@ -0,0 +1,21 @@
+import { Dimensions, PixelRatio } from 'react-native';
+
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const [shortDimension, longDimension] =
+    SCREEN_WIDTH < SCREEN_HEIGHT ? [SCREEN_WIDTH, SCREEN_HEIGHT] : [SCREEN_HEIGHT, SCREEN_WIDTH];
+
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+const scale = (size: number) =>
+    Math.round(
+        PixelRatio.roundToNearestPixel((shortDimension / guidelineBaseWidth) * (size as number)),
+    );
+
+const verticalScale = (size: number) =>
+    Math.round(
+        PixelRatio.roundToNearestPixel((longDimension / guidelineBaseHeight) * (size as number)),
+    );
+
+export { scale, verticalScale };
